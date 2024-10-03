a00ac25cfe25fe828ae3fc467f243b780db82efb
Frattini Luca <luca_frattini@outlook.com>
2024-10-03 13:29:26 +0200

    footer allows to send emails. Link to the GitHub repo.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,21 @@ import { Link } from "react-router-dom";
 function Footer() {
     return (
         <footer>
-            <Link to="/">Back Home</Link>
+            <div className="footer-content">
+                <a href="mailto:luca_frattini@outlook.com" className="footer-link">
+                luca_frattini@outlook.com
+                </a>
+                <span className="separator">|</span>
+                <a
+                href="https://github.com/LucaFrat/theusualpath"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-link"
+                >
+                View Source on GitHub
+                </a>
+            </div>
+            {/* <Link to="/">Back Home</Link> */}
             <p style={{fontSize:"0.7rem"}}>Copyright &copy; {new Date().getFullYear()} The Usual Path. All rights reserved.</p>
         </footer>
     );
